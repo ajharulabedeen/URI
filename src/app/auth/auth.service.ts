2f603f3fb769491fdfc7ffa5a7b37fcd9d2deec8
ajharul abedeen <ajharulabedeen@gmail.com>
2019-10-15 11:41:39 +0600

    sing up working with email exist error message

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   access_token: string;
   token_type: string;
   expires_in: number;
@@ -12,7 +12,7 @@ interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   signup(email: string, password: string) {
     return this.http
@@ -27,14 +27,29 @@ export class AuthService {
       )
       .pipe(
         catchError(errorRes => {
+          // console.log(errorRes.error.errors.email[0]);
           let errorMessage = 'An unknown error occurred!';
-          if (!errorRes.error || !errorRes.error.error) {
+          // errorMessage = 'This email already taken!';
+          // if (!errorRes.error || !errorRes.error.error) {
+          //   errorMessage = errorRes.error.errors.email[0];
+          //   return throwError(errorMessage);
+          // }
+
+          //working
+          // if (errorRes.error.errors.email[0] === "The email has already been taken.") {
+          if (errorRes.error.errors.email[0].match("The email has already been taken.")) {
+            errorMessage = 'This email already taken!';
+            console.log("ERR : " + errorRes.error.errors.email[0]);
+            errorMessage = errorRes.error.errors.email[0];
             return throwError(errorMessage);
           }
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email exists already';
-          }
+
+          // switch (errorRes.error.error.message) {
+          //   case 'EMAIL_EXISTS':
+          //     errorMessage = 'This email exists already';
+          // }
+
+
           return throwError(errorMessage);
         })
       );
