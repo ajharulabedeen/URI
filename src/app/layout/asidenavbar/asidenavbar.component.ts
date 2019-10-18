4ce109c08bef88ad6a823a18aed67f372d7d6d3b
ajharul abedeen <ajharulabedeen@gmail.com>
2019-10-18 15:40:35 +0600

    profile skeleton Done; education, work history table not added

--- src/app/layout/asidenavbar/asidenavbar.component.ts
+++ src/app/layout/asidenavbar/asidenavbar.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 })
 export class AsidenavbarComponent implements OnInit, OnDestroy {
 
-  isAuthenticated = false;
+  isAuthenticated = true;//have to make false
   private userSub: Subscription;
 
   profile = true;
