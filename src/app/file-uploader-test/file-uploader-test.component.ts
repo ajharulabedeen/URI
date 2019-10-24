7b26b798802f6c8f48093aba589905e4901ef096
ajharul abedeen <ajharulabedeen@gmail.com>
2019-10-24 21:10:07 +0600

    file test component working, with backEnd plus authentication

--- src/app/file-uploader-test/file-uploader-test.component.ts
+++ src/app/file-uploader-test/file-uploader-test.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 
 
 @Component({
@@ -28,7 +28,12 @@ export class FileUploaderTestComponent implements OnInit {
     // this.http is the injected HttpClient
   const uploadData = new FormData();
   uploadData.append('photo', this.selectedFile, this.selectedFile.name);
-  this.http.post('http://127.0.0.1:8000/api/t', uploadData)
+  // this.http.post('http://127.0.0.1:8000/api/t', uploadData,{
+  this.http.post('http://127.0.0.1:8000/api/uploadfile', uploadData,{
+    headers: new HttpHeaders({
+            'Authorization' : 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTU3MTkyODg2MywiZXhwIjoxNTcxOTMyNDYzLCJuYmYiOjE1NzE5Mjg4NjMsImp0aSI6ImN3aGdnQ0xRZG9nZlNzN2YiLCJzdWIiOjIsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEifQ.JI-XHdIbD1ry3ajrRECoCp_cnkMR-kPDKNhJD6c0ulk'
+          })
+  })
     .subscribe(event => {
       console.log(event); // handle event here
     });
